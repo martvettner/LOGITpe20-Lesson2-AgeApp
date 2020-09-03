7cc3f974e65f9cfae7d393d4bb03e4f707c771d8
Opilane TTHK <opilane@tthk.ee>
2020-09-03 11:59:53 +0300

    huumor

---
+++ AgeApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AgeApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mis su nimi on");
+            string name = Console.ReadLine();
+            Console.WriteLine("tere, "+ name);
+            Console.WriteLine("mis aastal sa sündisid?");
+            string UserInput;
+            UserInput = Console.ReadLine();
+            int SynniAasta;
+            SynniAasta = Int32.Parse(UserInput);
+                int age = 2020 - SynniAasta;
+                Console.WriteLine("sa oled " + age + " vana");
+
+
+
+
+        }
+    }
+}
